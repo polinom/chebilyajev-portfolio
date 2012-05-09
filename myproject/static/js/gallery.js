dc7aa3c2fc5413037f70f31941be2a9f15ec6d2a
Igor Polynets <polinom100@gmail.com>
2012-05-09 10:10:30 -0400

    fix gallery

--- myproject/static/js/gallery.js
+++ myproject/static/js/gallery.js
@@ -4,7 +4,15 @@ $(document).ready(function(){
 		 $('.loading_place').html('<img src="/static/images/ajax-loader.gif">')
          var big_img = $(this).attr('original')
          var img_el = $('.image_big')
-         img_el.attr('src', big_img)
+         img_el.fadeOut('fast',function(){img_el.fadeIn('slow'); img_el.attr('src', big_img)})
 	});
 	$('.image_big').load(function(){ $('.loading_place').html('') });
+
+	$('.nav_dot').click(function(e){
+		e.preventDefault()
+		var page = $(this).attr('page')
+		alert(page)
+
+
+	})
 })
