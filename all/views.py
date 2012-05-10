3899f786e7466afffbe30743faf983aef9655f9b
Igor Polynets <polinom100@gmail.com>
2012-05-10 00:10:55 -0400

    fix finish

--- all/views.py
+++ all/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from models import Image
 from django.shortcuts import get_list_or_404
+from django.utils import simplejson
+from django.http import HttpResponse
+from sorl.thumbnail import get_thumbnail
 
 TITLE_DICT = {'illystration':'illustration.png',
              'storyboard': 'storyboard.png',
              'character_design': 'char_design.png'
 }
+PER_PAGE = 18
+
 
 def gallery(request,gallery):
+    page = int(request.GET.get('page',0))
     images = get_list_or_404(Image, gallery=gallery)
-    pages = len(images)/float(18)
+    pages = len(images)/float(PER_PAGE)
     pages = int(pages) if pages - int(pages) == 0 else int(pages) + 1
     title_img = TITLE_DICT[gallery]
-    context = {'title':gallery, 'items': images[:18], 'first_item':images[0], 'title_img':title_img, 'pages':range(pages)}
-    return render(request, 'gallery.html', context)
+    images = images[PER_PAGE*page:PER_PAGE+(page*PER_PAGE)]
+    print images
+    if not request.is_ajax():
+        context = {'title':gallery, 'items': images[:PER_PAGE], 'first_item':images[0], 'title_img':title_img, 'pages':range(pages-1)}
+        return render(request, 'gallery.html', context)
+    else:
+    	data = [{'image':image.image.url,'thumb':get_thumbnail(image.image, '60x60', crop='center', quality=99).url} for image in images]
+    	return HttpResponse(simplejson.dumps(data),mimetype="application/json")
--- myproject/static/js/gallery.js
+++ myproject/static/js/gallery.js
@@ -1,17 +1,32 @@
 $(document).ready(function(){
-	$('.image_thumb').click(function(e){
+	$('.image_thumb').live('click', function(e){
 		 e.preventDefault();
 		 $('.loading_place').html('<img src="/static/images/ajax-loader.gif">')
          var big_img = $(this).attr('original')
          var img_el = $('.image_big')
          img_el.fadeOut('fast',function(){img_el.attr('src', big_img)})
 	});
+
 	$('.image_big').load(function(){ $('.loading_place').html(''); $('.image_big').fadeIn('slow'); });
 
-	$('.nav_dot').click(function(e){
+	$('.nav_dot').live('click',function(e){
 		e.preventDefault()
+		var img_list = $('.thumb_list')
+
+		$('.nav_dot').attr('src','/static/images/page_dor_black.png')
+		$(this).attr('src','/static/images/page_dor_white.png')
+
+
+		img_list.html('')
 		var page = $(this).attr('page')
-		alert(page)
+
+		$.get('',{'page':page},function(resp){
+          $(resp).each(function(index,item){
+          var img_html = '<img class="image_thumb" src="'+item.thumb+'" width="60" height="60" original="'+item.image+'">'
+           img_list.append(img_html)
+          })
+		})
+
 
 	})
 })
